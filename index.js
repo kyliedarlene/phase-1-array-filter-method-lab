4c747474150abfaae29a037b8b89a3eff1d77401
Kylie Clark <kyliedarlene@icloud.com>
2024-01-01 16:42:16 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((element) => element.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter((element) => element[0] === name[0])
+}
+
+function matchName(drivers, nameToFind) {
+    return drivers.filter((element) => element.name === nameToFind)
+}
+
+// console.log(["Beck", "Bowie", "Indy"].filter((element) => element === "Indy"));
+console.log(findMatching(["Beck", "Bowie", "Indy"], "Indy"));
